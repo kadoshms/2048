7b9eab1f99aa4b36705951c7002eb95555b665e2
kadoshms <kadoshms@gmail.com>
2016-05-21 23:11:00 +0300

    global constants file class

---
+++ media/js/classes/consts.js
@@ -0,0 +1,20 @@
+/**
+ * Created by mor on 19/05/16.
+ */
+
+
+define([], function(){
+    
+    var consts = {};
+    
+    consts.KEY_DELAY = 100;
+
+    // keyboard codes
+    consts.RIGHT = 39;
+    consts.LEFT = 37;
+    consts.UP = 38;
+    consts.DOWN = 40;
+    
+    return consts;
+    
+});
